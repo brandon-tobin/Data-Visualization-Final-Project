6fe2482068a88477f2a1e8dbe7882df615975565
Brandon Tobin <brandontobin@cox.net>
2016-12-02 21:08:55 -0700

    Removed the yearchart div correctly this time

--- WorkingVis/public/js/wordCloud.js
+++ WorkingVis/public/js/wordCloud.js
@@ -15,18 +15,6 @@ function WordCloud(worldChart) {
 WordCloud.prototype.init = function(){
 
     var self = this;
-    self.margin = {top: 10, right: 20, bottom: 30, left: 50};
-    var divyearChart = d3.select("#year-chart").classed("fullView", true);
-
-    //Gets access to the div element created for this chart from HTML
-    self.svgBounds = divyearChart.node().getBoundingClientRect();
-    self.svgWidth = self.svgBounds.width - self.margin.left - self.margin.right;
-    self.svgHeight = 100;
-
-    //creates svg element within the div
-    self.svg = divyearChart.append("svg")
-        .attr("width",self.svgWidth)
-        .attr("height",self.svgHeight)
 
     createYear();
 };
